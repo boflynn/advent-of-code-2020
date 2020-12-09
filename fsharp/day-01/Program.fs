de4cae4503ad994ff79f4cacf2b8b652574f65ff
Bo Flynn <boflynn@gmail.com>
2020-12-08 21:09:27 -0500

    Small cleanup to prepare for part 2

--- fsharp/day-01/Program.fs
+++ fsharp/day-01/Program.fs
@@ -1,13 +1,22 @@
 let readLines filePath = System.IO.File.ReadLines(filePath)
 let generateComplement intValue = 2020 - intValue
 
-[<EntryPoint>]
-let main argv =
+let numbersFromFile = 
     let lines = readLines "./input.txt"
     let ints = lines |> Seq.map(int)
-    let complements = ints |> Seq.map(generateComplement)
-    let matches = Set.intersect (Set.ofSeq ints) (Set.ofSeq complements)
-    let multiplication = matches |> Set.fold (*) 1
+    ints
 
-    printf "%i" multiplication
+let twoNumbers = 
+    let ints = numbersFromFile
+    let complements = numbersFromFile |> Seq.map(generateComplement)
+    let matches = Set.intersect (Set.ofSeq ints) (Set.ofSeq complements)
+    matches
+
+let setMultiplication numbers = numbers |> Set.fold (*) 1
+
+[<EntryPoint>]
+let main argv =
+    let result = twoNumbers |> setMultiplication
+
+    printf "%i" result
     0
