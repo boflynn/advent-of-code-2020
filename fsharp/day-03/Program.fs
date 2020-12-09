1f96876aab2bad493b1dbd64dc7fe65f12643e79
Bo Flynn <boflynn@gmail.com>
2020-12-08 23:55:14 -0500

    This seemed easier

--- fsharp/day-03/Program.fs
+++ fsharp/day-03/Program.fs
@@ -2,14 +2,27 @@ let readLines filePath = System.IO.File.ReadLines(filePath)
 let readArray filePath = readLines filePath |> Seq.map(fun(a) -> a.ToCharArray())
                                             |> Seq.toList
 
+let listMultiplication numbers = numbers |> List.fold (*) 1L
+
 [<EntryPoint>]
 let main argv =
     let array = readArray "./input.txt"
     let width = array.[0].Length
+    let patterns = [(1, 1); (1, 3); (1, 5); (1, 7); (2, 1)]
 
-    let trees = array |> List.indexed
-                |> List.filter (fun(a, b) -> b.[a * 3 % width] = '#')
-                |> List.length
+    let treeCounts = patterns |> List.map(fun(pattern) -> 
+        let down = fst pattern
+        let right = snd pattern
 
-    printfn "%i" trees
+        let treeCount = array |> List.indexed
+                              |> List.filter (fun(a, b) -> a % down = 0 && b.[a / down * right % width] = '#')
+                              |> List.length
+                              |> int64
+
+        treeCount
+    )
+
+    let result = listMultiplication treeCounts
+
+    printfn "%u" result
     0
