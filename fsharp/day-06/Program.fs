2119fc1bbc76a00f9415839a9f9f9f440674ad30
Bo Flynn <boflynn@gmail.com>
2020-12-11 01:08:30 -0500

    Day 6 Part 2
    I don't feel great about using Seq.zip, but it works.

--- fsharp/day-06/Program.fs
+++ fsharp/day-06/Program.fs
@@ -1,12 +1,20 @@
 let readLines = System.IO.File.ReadAllText("./input.txt")
 let answerGroups = readLines.Split("\n\n")
+let groupPersonCounts = answerGroups |>
+                                     Array.map(fun a -> a.Split("\n") |> Array.length)
 let answerStrings = answerGroups
                       |> Seq.map(fun(a) -> a.Replace('\n', ' ').Replace(" ", ""))
 
 [<EntryPoint>]
 let main argv =
-    answerStrings |> Seq.map(Seq.distinct)
-                  |> Seq.map(Seq.length)
-                  |> Seq.sum
-                  |> printfn "%A"
+    let combined = answerStrings
+                   |> Seq.zip groupPersonCounts
+                   |> Seq.map(fun (a, b) -> let groups = b |> Seq.countBy (id)
+                                            let allAnswered = groups |> Seq.filter(fun(c, count) -> count = a)
+
+                                            allAnswered |> Seq.length
+                   )
+
+    combined |> Seq.sum |> printfn "%A"
+    
     0
